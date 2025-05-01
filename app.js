827e38ff40bffb4d81cea0e1ca81b6bf2eb46f6d
S-entinel <106978598+schwarzsch1ld@users.noreply.github.com>
2025-05-01 11:38:13 +0100

    created get api/topics

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const endpointsJson = require("./endpoints.json");
+const { getTopics } = require("./controllers");
+const { handlePsqlErrors, handleCustomErrors, handleServerErrors } = require("./errors");
+
 
 app.use(express.json());
 
@@ -11,5 +14,16 @@ app.get("/", (req, res) => {
 app.get("/api", (req, res) => {
   res.status(200).send({ endpoints: endpointsJson });
 });
+
+app.get("/api/topics", getTopics);
+
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "Route not found" });
+});
+
+app.use(handlePsqlErrors);
+app.use(handleCustomErrors);
+app.use(handleServerErrors);
+
 
 module.exports = app;
