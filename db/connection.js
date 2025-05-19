77935453288eb3470039942265cba33a44b62001
S-entinel <106978598+schwarzsch1ld@users.noreply.github.com>
2025-05-19 11:15:25 +0100

    .

--- db/connection.js
+++ db/connection.js
@@ -2,9 +2,19 @@ const { Pool } = require("pg");
 
 const ENV = process.env.NODE_ENV || 'development'
 
+const config = {};
+
 require('dotenv').config({path: `${__dirname}/../.env.${ENV}`})
 
 // const db = new Pool();
+
+
+if (ENV === "production") {
+    config.connectionString = process.env.DATABASE_URL;
+    config.max = 2;
+  }
+
+const db = new Pool(config)
 
 
 if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
@@ -13,14 +23,7 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
     console.log(`Connected to ${process.env.PGDATABASE}`)
 }
 
-const config = {};
-
-if (ENV === "production") {
-    config.connectionString = process.env.DATABASE_URL;
-    config.max = 2;
-  }
-
-module.exports = new Pool(config)
+module.exports = db;
 
 
 // module.exports = db;
